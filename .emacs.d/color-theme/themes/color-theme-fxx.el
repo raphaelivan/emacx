99470eaf9125497bb2fd47c10861a7c46186de70
Marcos Piccinini <x@nofxx.com>
2011-03-04 23:02:27 -0300

    color theme

--- .emacs.d/color-theme/themes/color-theme-fxx.el
+++ .emacs.d/color-theme/themes/color-theme-fxx.el
@@ -1,35 +1,79 @@
-(eval-when-compile
-  (require 'color-theme))
+;; Fxx Colour Theme for Emacs.
+;;
+;; Defines a colour scheme resembling that of the original TextMate Fxx colour theme.
+;; To use add the following to your .emacs file (requires the color-theme package):
+;;
+;; (require 'color-theme)
+;; (color-theme-initialize)
+;; (load-file "~/.emacs.d/fxx-emacs/color-theme-fxx.el")
+;;
+;; And then (color-theme-fxx) to activate it.
+;;
+;; Several areas still require improvement such as recognition of code that ruby-mode doesn't
+;; yet pick up (eg. parent classes), Rails/Merb keywords, or non Ruby code related areas
+;; (eg. dired, HTML, etc). Please feel free to customize further and send in any improvements,
+;; patches most welcome.
+;;
+;; MIT License Copyright (c) 2008 Marcus Crafter <crafterm@redartisan.com>
+;; Credits due to the excellent TextMate Fxx theme
+;;
 
 (defun color-theme-fxx ()
-  "Color theme by nofxx (2011-03-03) based on sunburst by dngpng, created 2007-09-11."
+  "Color theme by Marcus Crafter, based off the TextMate Fxx theme, created 2008-04-18"
   (interactive)
   (color-theme-install
    '(color-theme-fxx
-     ((background-color . "#111")
+     ((background-color . "black")
       (background-mode . dark)
-      (border-color . "#111")
-      (cursor-color . "yellow")
-      (foreground-color . "#ddd")
+      (border-color . "black")
+      (cursor-color . "#A7A7A7")
+      (foreground-color . "#F8F8F8")
       (mouse-color . "sienna1"))
-     (default ((t (:background "#111" :foreground "#ddd"))))
+     (default ((t (:background "black" :foreground "white"))))
      (blue ((t (:foreground "blue"))))
      (bold ((t (:bold t))))
-     (bold-italic ((t (:bold t :slant italic))))
+     (bold-italic ((t (:bold t))))
      (border-glyph ((t (nil))))
-     (buffers-tab ((t (:background "#111" :foreground "#ddd"))))
+     (buffers-tab ((t (:background "black" :foreground "white"))))
      (font-lock-builtin-face ((t (:foreground "#dd7b3b"))))
      (font-lock-comment-face ((t (:foreground "#666" ))))
      (font-lock-constant-face ((t (:foreground "#99cf50"))))
      (font-lock-doc-string-face ((t (:foreground "#9b859d"))))
-     (font-lock-function-name-face ((t (:foreground "#e9c062" :bold t))))
-     (font-lock-keyword-face ((t (:foreground "#cf6a4c" :bold t))))
+     (font-lock-function-name-face ((t (:foreground "#e9c062" :bold t))))  ;; fnama
+     (font-lock-keyword-face ((t (:foreground "#bb2222" :bold t))))        ;; keyword
      (font-lock-preprocessor-face ((t (:foreground "#aeaeae"))))
-     (font-lock-reference-face ((t (:foreground "8b98ab"))))
-     (font-lock-string-face ((t (:foreground "#A5C261"))))
-     (font-lock-type-face ((t (:foreground "#c5af75"))))
-     (font-lock-variable-name-face ((t (:foreground "#3387cc"))))
+     (font-lock-reference-face ((t (:foreground "ff0000"))))               ;; ??
+     (font-lock-string-face ((t (:foreground "#55cc55"))))                 ;; string
+     (font-lock-type-face ((t (:foreground "#FC9C00"))))                   ;; Capital, coffee var?
+     (font-lock-variable-name-face ((t (:foreground "#3387cc"))))          ;; @this
      (font-lock-warning-face ((t (:bold t :background "#420e09" :foreground "#eeeeee"))))
+
+     (font-lock-regexp-grouping-backslash ((t (:foreground "#E9C062"))))
+     (font-lock-regexp-grouping-construct ((t (:foreground "red"))))
+
+     (gui-element ((t (:background "#0e2231" :foreground "black"))))
+     (highlight ((t (:bold t :slant italic))))
+     (highline-face ((t (:background "#4a410d"))))
+     (italic ((t (nil))))
+     (left-margin ((t (nil))))
+     (mmm-default-submode-face ((t (:background "#111"))))
+     (mode-line ((t (:background "#e6e5e4" :foreground "black"))))
+     (primary-selection ((t (:background "#222"))))
+     (region ((t (:background "#4a410d"))))
+     (text-cursor ((t (:background "yellow" :foreground "black"))))
+     (underline ((nil (:underline nil))))
+
+     (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
+     (region ((t (:background "#27292A"))))
+     (mode-line ((t (:background "grey75" :foreground "black"))))
+     (highlight ((t (:background "#222222"))))
+     (highline-face ((t (:background "SeaGreen"))))
+     (italic ((t (nil))))
+     (left-margin ((t (nil))))
+     (text-cursor ((t (:background "yellow" :foreground "black"))))
+     (toolbar ((t (nil))))
+     (underline ((nil (:underline nil))))
+
      (erc-current-nick-face ((t (:foreground "#aeaeae"))))
      (erc-default-face ((t (:foreground "#ddd"))))
      (erc-keyword-face ((t (:foreground "#cf6a4c"))))
@@ -63,14 +107,5 @@
      (magit-branch ((t (:foreground "#e9c062"))))
      (magit-section-title ((t (:foreground "#9b859d"))))
      (magit-item-highlight ((t (:background "#1f1f1f"))))
-     (gui-element ((t (:background "#0e2231" :foreground "black"))))
-     (highlight ((t (:bold t :slant italic))))
-     (highline-face ((t (:background "#4a410d"))))
-     (italic ((t (nil))))
-     (left-margin ((t (nil))))
-     (mmm-default-submode-face ((t (:background "#111"))))
-     (mode-line ((t (:background "#e6e5e4" :foreground "black"))))
-     (primary-selection ((t (:background "#222"))))
-     (region ((t (:background "#4a410d"))))
-     (text-cursor ((t (:background "yellow" :foreground "black"))))
-     (underline ((nil (:underline nil)))))))
+
+     (zmacs-region ((t (:background "snow" :foreground "ble")))))))
