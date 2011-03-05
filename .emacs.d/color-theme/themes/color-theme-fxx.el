603bee1e1c2d7572818cd0353a9629a6b733b05e
Marcos Piccinini <x@nofxx.com>
2011-03-05 00:01:04 -0300

    theme changes

--- .emacs.d/color-theme/themes/color-theme-fxx.el
+++ .emacs.d/color-theme/themes/color-theme-fxx.el
@@ -23,27 +23,27 @@
   (interactive)
   (color-theme-install
    '(color-theme-fxx
-     ((background-color . "black")
+     ((background-color . "#111")
       (background-mode . dark)
       (border-color . "black")
       (cursor-color . "#A7A7A7")
-      (foreground-color . "#F8F8F8")
+      (foreground-color . "#eee")
       (mouse-color . "sienna1"))
-     (default ((t (:background "black" :foreground "white"))))
+     (default ((t (:background "#111" :foreground "#eee"))))
      (blue ((t (:foreground "blue"))))
      (bold ((t (:bold t))))
      (bold-italic ((t (:bold t))))
      (border-glyph ((t (nil))))
      (buffers-tab ((t (:background "black" :foreground "white"))))
      (font-lock-builtin-face ((t (:foreground "#dd7b3b"))))
-     (font-lock-comment-face ((t (:foreground "#666" ))))
-     (font-lock-constant-face ((t (:foreground "#99cf50"))))
+     (font-lock-comment-face ((t (:foreground "#666" ))))                  ;; comment
+     (font-lock-constant-face ((t (:foreground "#aa2222"))))               ;; symbol
      (font-lock-doc-string-face ((t (:foreground "#9b859d"))))
-     (font-lock-function-name-face ((t (:foreground "#e9c062" :bold t))))  ;; fnama
+     (font-lock-function-name-face ((t (:foreground "#e9c062" :bold t))))  ;; fname
      (font-lock-keyword-face ((t (:foreground "#bb2222" :bold t))))        ;; keyword
      (font-lock-preprocessor-face ((t (:foreground "#aeaeae"))))
-     (font-lock-reference-face ((t (:foreground "ff0000"))))               ;; ??
-     (font-lock-string-face ((t (:foreground "#55cc55"))))                 ;; string
+     (font-lock-reference-face ((t (:foreground "ff00ff"))))               ;; ??
+     (font-lock-string-face ((t (:foreground "#188030" :background "#1c1c1c"))))                    ;; string
      (font-lock-type-face ((t (:foreground "#FC9C00"))))                   ;; Capital, coffee var?
      (font-lock-variable-name-face ((t (:foreground "#3387cc"))))          ;; @this
      (font-lock-warning-face ((t (:bold t :background "#420e09" :foreground "#eeeeee"))))
@@ -51,26 +51,18 @@
      (font-lock-regexp-grouping-backslash ((t (:foreground "#E9C062"))))
      (font-lock-regexp-grouping-construct ((t (:foreground "red"))))
 
-     (gui-element ((t (:background "#0e2231" :foreground "black"))))
      (highlight ((t (:bold t :slant italic))))
      (highline-face ((t (:background "#4a410d"))))
      (italic ((t (nil))))
      (left-margin ((t (nil))))
      (mmm-default-submode-face ((t (:background "#111"))))
-     (mode-line ((t (:background "#e6e5e4" :foreground "black"))))
      (primary-selection ((t (:background "#222"))))
-     (region ((t (:background "#4a410d"))))
-     (text-cursor ((t (:background "yellow" :foreground "black"))))
-     (underline ((nil (:underline nil))))
+     (region ((t (:background "#333")))) ;; suave e continuo!
 
      (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
-     (region ((t (:background "#27292A"))))
      (mode-line ((t (:background "grey75" :foreground "black"))))
      (highlight ((t (:background "#222222"))))
-     (highline-face ((t (:background "SeaGreen"))))
-     (italic ((t (nil))))
-     (left-margin ((t (nil))))
-     (text-cursor ((t (:background "yellow" :foreground "black"))))
+     (text-cursor ((t (:background "#aaa" :foreground "black"))))
      (toolbar ((t (nil))))
      (underline ((nil (:underline nil))))
 
