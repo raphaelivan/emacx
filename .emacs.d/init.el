56ba8353a5d41a9b2e04931f44817c40e7c2c665
Marcos Piccinini <x@nofxx.com>
2011-03-04 22:07:11 -0300

    initel

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -161,43 +161,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;
-;;
 ;;   Ruby
 ;;
+;;
+;; Part of the Emacs Starter Kit
 ;;
 (autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
 (setq auto-mode-alist  (cons '(".rb$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist  (cons '(".erb$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist  (cons '(".god$" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist  (cons '("Gemfile$" . ruby-mode) auto-mode-alist))
-;; Rake files are ruby, too, as are gemspecs.
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
-(add-hook 'ruby-mode-hook
-  (lambda()
-    (add-hook 'local-write-file-hooks
-      '(lambda()
-        (save-excursion
-              (untabify (point-min) (point-max))
-              (delete-trailing-whitespace)
-                )
-          )
-        )
-        (set (make-local-variable 'indent-tabs-mode) 'nil)
-        (set (make-local-variable 'tab-width) 2)
-        (imenu-add-to-menubar "IMENU")
-        (define-key ruby-mode-map "\C-m" 'newline-and-indent)
-        (require 'ruby-electric)
-        (ruby-electric-mode t)
-  )
-)
 
-;; Part of the Emacs Starter Kit
 (eval-after-load 'ruby-mode
   '(progn
-;;     (require 'ruby-compilation)
+     ;; work around possible elpa bug
+     (ignore-errors (require 'ruby-compilation))
      (setq ruby-use-encoding-map nil)
      (add-hook 'ruby-mode-hook 'inf-ruby-keys)
      (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
@@ -207,22 +185,31 @@
 
 (global-set-key (kbd "C-h r") 'ri)
 
+;; Rake files are ruby, too, as are gemspecs, rackup files, etc.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
 
 ;;; Rake
+
 (defun pcomplete/rake ()
   "Completion rules for the `ssh' command."
   (pcomplete-here (pcmpl-rake-tasks)))
 
 (defun pcmpl-rake-tasks ()
-   "Return a list of all the rake tasks defined in the current
+  "Return a list of all the rake tasks defined in the current
 projects. I know this is a hack to put all the logic in the
 exec-to-string command, but it works and seems fast"
-   (delq nil (mapcar '(lambda(line)
-      (if (string-match "rake \\([^ ]+\\)" line) (match-string 1 line)))
-     (split-string (shell-command-to-string "rake -T") "[\n]"))))
+  (delq nil (mapcar '(lambda(line)
+                       (if (string-match "rake \\([^ ]+\\)" line) (match-string 1 line)))
+                    (split-string (shell-command-to-string "rake -T") "[\n]"))))
 
 (defun rake (task)
   (interactive (list (completing-read "Rake (default: default): "
@@ -231,47 +218,81 @@ exec-to-string command, but it works and seems fast"
 
 
 ;; Clear the compilation buffer between test runs.
-;;(eval-after-load 'ruby-compilation
-;;  '(progn
-;;     (defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
-;;       (let ((comp-buffer-name (format "*%s*" name)))
-;;         (when (get-buffer comp-buffer-name)
-;;           (with-current-buffer comp-buffer-name
-;;             (delete-region (point-min) (point-max))))))
-;;     (ad-activate 'ruby-do-run-w/compilation)))
+(eval-after-load 'ruby-compilation
+  '(progn
+     (defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
+       (let ((comp-buffer-name (format "*%s*" name)))
+         (when (get-buffer comp-buffer-name)
+           (with-current-buffer comp-buffer-name
+             (delete-region (point-min) (point-max))))))
+     (ad-activate 'ruby-do-run-w/compilation)))
 
-(add-hook 'ruby-mode-hook 'coding-hook)
+(add-hook 'ruby-mode-hook 'run-coding-hook)
 
 ;;; Flymake
 
-;; (eval-after-load 'ruby-mode
-;;   '(progn
-;;      (require 'flymake)
+(defun flymake-ruby-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    ;; Invoke ruby with '-c' to get syntax checking
+    (list "ruby" (list "-c" local-file))))
 
-;;      ;; Invoke ruby with '-c' to get syntax checking
-;;      (defun flymake-ruby-init ()
-;;        (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                           'flymake-create-temp-inplace))
-;;               (local-file (file-relative-name
-;;                            temp-file
-;;                            (file-name-directory buffer-file-name))))
-;;          (list "ruby" (list "-c" local-file))))
+(defun flymake-ruby-enable ()
+  (when (and buffer-file-name
+             (file-writable-p
+              (file-name-directory buffer-file-name))
+             (file-writable-p buffer-file-name)
+             (if (fboundp 'tramp-list-remote-buffers)
+                 (not (subsetp
+                       (list (current-buffer))
+                       (tramp-list-remote-buffers)))
+               t))
+    (local-set-key (kbd "C-c d")
+                   'flymake-display-err-menu-for-current-line)
+    (flymake-mode t)))
 
-;;      (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;;      (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(eval-after-load 'ruby-mode
+  '(progn
+     (require 'flymake)
+     (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+     (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
+           flymake-err-line-patterns)
+     (add-hook 'ruby-mode-hook 'flymake-ruby-enable)))
 
-;;      (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-;;            flymake-err-line-patterns)
+;; Rinari (Minor Mode for Ruby On Rails)
+(setq rinari-major-modes
+      (list 'mumamo-after-change-major-mode-hook 'dired-mode-hook 'ruby-mode-hook
+            'css-mode-hook 'yaml-mode-hook 'javascript-mode-hook))
 
-;;      (add-hook 'ruby-mode-hook
-;;                (lambda ()
-;;                  (when (and buffer-file-name
-;;                             (file-writable-p
-;;                              (file-name-directory buffer-file-name))
-;;                             (file-writable-p buffer-file-name))
-;;                    (local-set-key (kbd "C-c d")
-;;                                   'flymake-display-err-menu-for-current-line)
-;;                    (flymake-mode t))))))
+;; TODO: set up ri
+;; TODO: electric
+;; Rake files are ruby, too, as are gemspecs.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
+;; (add-hook 'ruby-mode-hook
+;;   (lambda()
+;;     (add-hook 'local-write-file-hooks
+;;       '(lambda()
+;;         (save-excursion
+;;               (untabify (point-min) (point-max))
+;;               (delete-trailing-whitespace)
+;;                 )
+;;           )
+;;         )
+;;         (set (make-local-variable 'indent-tabs-mode) 'nil)
+;;         (set (make-local-variable 'tab-width) 2)
+;;         (imenu-add-to-menubar "IMENU")
+;;         (define-key ruby-mode-map "\C-m" 'newline-and-indent)
+;;         (require 'ruby-electric)
+;;         (ruby-electric-mode t)
+;;   )
+;; )
 
 
 
@@ -282,8 +303,8 @@ exec-to-string command, but it works and seems fast"
 ;;    Emacs-Rails
 ;;
 ;;
-(load "find-recursive")
-(require 'rails)
+;;(load "find-recursive")
+(require 'rails)                        ;
 
 ; Rinari (RAILS) Configurações
 ;; (require 'rinari)
