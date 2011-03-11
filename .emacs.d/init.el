cdb5c48b1cb4e2aa68a11ff592ac04bb86d4f561
Marcos Piccinini <x@nofxx.com>
2011-03-11 02:53:23 -0300

    elpa issue..:

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,7 +121,7 @@
 
 ;;
 ;; Perspective
-(persp-mode)
+;;(persp-mode)
 
 (require 'paren) (show-paren-mode t)
 
@@ -295,24 +295,24 @@ exec-to-string command, but it works and seems fast"
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
-;; (add-hook 'ruby-mode-hook
-;;   (lambda()
-;;     (add-hook 'local-write-file-hooks
-;;       '(lambda()
-;;         (save-excursion
-;;               (untabify (point-min) (point-max))
-;;               (delete-trailing-whitespace)
-;;                 )
-;;           )
-;;         )
-;;         (set (make-local-variable 'indent-tabs-mode) 'nil)
-;;         (set (make-local-variable 'tab-width) 2)
-;;         (imenu-add-to-menubar "IMENU")
-;;         (define-key ruby-mode-map "\C-m" 'newline-and-indent)
-;;         (require 'ruby-electric)
-;;         (ruby-electric-mode t)
-;;   )
-;; )
+(add-hook 'ruby-mode-hook
+  (lambda()
+    (add-hook 'local-write-file-hooks
+      '(lambda()
+        (save-excursion
+              (untabify (point-min) (point-max))
+              (delete-trailing-whitespace)
+                )
+          )
+        )
+        (set (make-local-variable 'indent-tabs-mode) 'nil)
+        (set (make-local-variable 'tab-width) 2)
+        (imenu-add-to-menubar "IMENU")
+        (define-key ruby-mode-map "\C-m" 'newline-and-indent)
+        (require 'ruby-electric)
+        (ruby-electric-mode t)
+  )
+)
 
 
 
