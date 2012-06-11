7baa0cea90487973a4044b76bd2eae73a1362f35
Raphael Ivan <raphaivan@gmail.com>
2012-06-11 19:31:24 -0300

    from Inconsolata to Monaco

--- .emacs.d/osx.el
+++ .emacs.d/osx.el
@@ -9,7 +9,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#111" :foreground "#eee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 240 :width normal :foundry "apple" :family "Inconsolata"))))
+ '(default ((t (:inherit nil :stipple nil :background "#111" :foreground "#eee" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "monaco"))))
  '(mumamo-background-chunk-major ((t (:background "black"))))
  '(mumamo-background-chunk-submode1 ((t (:background "gray12")))))
 
